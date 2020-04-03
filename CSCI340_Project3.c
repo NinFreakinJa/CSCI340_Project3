896ce85bd694ee09bfcc5687e793a07c2552ffda
Reid Foster <j.reid.foster@gmail.com>
2020-04-03 00:08:31 -0400

    Update CSCI340_Project3.c

--- CSCI340_Project3.c
+++ CSCI340_Project3.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <semaphore.h>
 
 #include "queue.c"
 
@@ -19,59 +18,12 @@ pthread_mutex_t linecountlock;
 pthread_mutex_t wordcountlock;
 pthread_mutex_t donelock;
 pthread_cond_t c= PTHREAD_COND_INITIALIZER;
-sem_t s;
 int done;
 
-
-int wordCounter(char* str,int size){
-    return -1;
-}
-
-int checkIfDone(){
-    pthread_mutex_lock(&linecountlock);
-    pthread_mutex_lock(&donelock);
-    if(lineCounter>0&&done==0){
-        lineCounter--;
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 1;
-    }
-    else if(lineCounter<=0 && done==0){
-        pthread_mutex_unlock(&donelock);
-        pthread_cond_wait(&c,&linecountlock);
-        pthread_mutex_unlock(&linecountlock);
-        checkIfDone();
-    }
-    else if(lineCounter>0 && done==1){
-        lineCounter--;
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 1;
-    }
-    else{
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 0;
-    }
-}
-
-void *consumer(void* thnum){
-    int thId=*((int*)thnum);
-    while(checkIfDone()==1){
-        char* value=NULL;
-        int size=0;
-        Queue_Dequeue(&lineQueue,&value,&size);
-        int count=wordCounter(value,size);
-        pthread_mutex_lock(&wordcountlock);
-        totalWC+=count;
-        pthread_mutex_unlock(&wordcountlock);
-        printf("Thread %d:  %s  : Word Count=%d\n",thId,value,count);
-    }
-    return NULL;
-}
-
-
+int checkIfDone();
+void *consumer(void* thnum);
 int isAlpha(char c);
+int wordCounter(char* str,int size);
 
 int main(int argc, char const *argv[]){
     // Checking command-line arguments.
@@ -84,8 +36,7 @@ int main(int argc, char const *argv[]){
     Queue_Init(&lineQueue);
     pthread_mutex_init(&linecountlock, NULL);
     pthread_mutex_init(&wordcountlock, NULL);
-    pthread_mutex_init(&donelock, NULL);
-    sem_init(&s,0,0);
+    pthread_mutex_init(&donelock, NULL);;
     //FILE *fp;
     char *line=NULL;
     size_t len=0;
@@ -112,9 +63,9 @@ int main(int argc, char const *argv[]){
         char* curr=malloc(len);
         int size=len;
         // Stripping newline character.
-        if((pos=strchr(line, '\n')) != NULL){
+        /*if((pos=strchr(line, '\n')) != NULL){
             *pos = '\0';
-        }
+        }*/
         strcpy(curr,line);
         Queue_Enqueue(&lineQueue,&curr,&size);
         pthread_mutex_lock(&linecountlock);
@@ -125,7 +76,6 @@ int main(int argc, char const *argv[]){
     pthread_mutex_lock(&donelock);
     done=1;
     pthread_mutex_unlock(&donelock);
-    //sem_post(&s);
     for(int i=0;i<consumerTaskCount;i++){
         pthread_join(p[i],NULL);
     }
@@ -137,12 +87,27 @@ int main(int argc, char const *argv[]){
 
 int checkIfDone(){
     pthread_mutex_lock(&linecountlock);
-    if(lineCounter>0){
+    pthread_mutex_lock(&donelock);
+    if(lineCounter>0&&done==0){
         lineCounter--;
+        pthread_mutex_unlock(&donelock);
+        pthread_mutex_unlock(&linecountlock);
+        return 1;
+    }
+    else if(lineCounter<=0 && done==0){
+        pthread_mutex_unlock(&donelock);
+        pthread_cond_wait(&c,&linecountlock);
+        pthread_mutex_unlock(&linecountlock);
+        checkIfDone();
+    }
+    else if(lineCounter>0 && done==1){
+        lineCounter--;
+        pthread_mutex_unlock(&donelock);
         pthread_mutex_unlock(&linecountlock);
         return 1;
     }
     else{
+        pthread_mutex_unlock(&donelock);
         pthread_mutex_unlock(&linecountlock);
         return 0;
     }
