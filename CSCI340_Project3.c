6a35d0192e43084a2ad4226e1a37a1369c8b0908
Ethan Guthrie <guthrieec@cofc.edu>
2020-04-03 00:01:22 -0400

    I am stuff

--- CSCI340_Project3.c
+++ CSCI340_Project3.c
@@ -22,56 +22,10 @@ pthread_cond_t c= PTHREAD_COND_INITIALIZER;
 sem_t s;
 int done;
 
-
-int wordCounter(char* str,int size){
-    return -1;
-}
-
-int checkIfDone(){
-    pthread_mutex_lock(&linecountlock);
-    pthread_mutex_lock(&donelock);
-    if(lineCounter>0&&done==0){
-        lineCounter--;
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 1;
-    }
-    else if(lineCounter<=0 && done==0){
-        pthread_mutex_unlock(&donelock);
-        pthread_cond_wait(&c,&linecountlock);
-        pthread_mutex_unlock(&linecountlock);
-        checkIfDone();
-    }
-    else if(lineCounter>0 && done==1){
-        lineCounter--;
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 1;
-    }
-    else{
-        pthread_mutex_unlock(&donelock);
-        pthread_mutex_unlock(&linecountlock);
-        return 0;
-    }
-}
-
-void *consumer(void* thnum){
-    int thId=*((int*)thnum);
-    while(checkIfDone()==1){
-        char* value=NULL;
-        int size=0;
-        Queue_Dequeue(&lineQueue,&value,&size);
-        int count=wordCounter(value,size);
-        pthread_mutex_lock(&wordcountlock);
-        totalWC+=count;
-        pthread_mutex_unlock(&wordcountlock);
-        printf("Thread %d:  %s  : Word Count=%d\n",thId,value,count);
-    }
-    return NULL;
-}
-
-
+int checkIfDone();
+void *consumer(void* thnum);
 int isAlpha(char c);
+int wordCounter(char* str,int size);
 
 int main(int argc, char const *argv[]){
     // Checking command-line arguments.
